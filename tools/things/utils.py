ee9dced2fcd56d3856521e8606240d1a344f8c42
florianmahner <florian.mahner@icloud.com>
2025-02-05 18:14:14 +0100

    Make concise similarity datasets using factories

--- tools/things/utils.py
+++ tools/things/utils.py
@@ -20,7 +20,7 @@ def sort_concepts(concepts: pd.DataFrame) -> np.ndarray:
 
 
 def filter_rsm_by_things_concepts(
-    rsm_human: np.ndarray, concepts: pd.DataFrame
+    rsm_human: np.ndarray, concepts: pd.DataFrame, rank_sort: bool = True
 ) -> tuple[np.ndarray, np.ndarray]:
     """Sort human and DNN by their assigned concept concepts, so that
     objects belonging to the same concept are grouped together in the RSM"""
@@ -32,5 +32,37 @@ def filter_rsm_by_things_concepts(
 
     rsm_human = rsm_human[sorted_items, :]
     rsm_human = rsm_human[:, sorted_items]
-    rsm_human = rankdata(rsm_human).reshape(rsm_human.shape)
+    if rank_sort:
+        rsm_human = rankdata(rsm_human).reshape(rsm_human.shape)
     return rsm_human
+
+
+def filter_concepts_by_cls_names(
+    concepts: pd.DataFrame, cls_names: np.ndarray
+) -> np.ndarray:
+    """Filter by cls names."""
+    # Create a copy of cls_names to preserve the original order
+    modified_cls_names = cls_names.copy()
+
+    # Remove "camera" and "file" from the copy
+    modified_cls_names = modified_cls_names[
+        (modified_cls_names != "camera") & (modified_cls_names != "file")
+    ]
+
+    # Append "camera1" and "file1" to the end of the copy
+    modified_cls_names = np.append(modified_cls_names, ["camera1", "file1"])
+
+    # Replace white space with underscore in modified_cls_names
+    modified_cls_names = [s.replace(" ", "_") for s in modified_cls_names]
+
+    # Create a dictionary to map modified_cls_names to their indices
+    name_to_index = {name: index for index, name in enumerate(modified_cls_names)}
+
+    # Filter concepts and sort them based on the order in modified_cls_names
+    filtered_concepts = concepts[concepts["uniqueID"].isin(modified_cls_names)]
+    filtered_concepts = filtered_concepts.sort_values(
+        by="uniqueID", key=lambda x: x.map(name_to_index)
+    )
+
+    indices = filtered_concepts.index
+    return indices
